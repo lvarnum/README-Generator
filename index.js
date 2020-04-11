2ad31712b28921660c47d01ae0bf65243124c448
lvarnum <lvarnum@email.arizona.edu>
2020-04-11 15:50:08 -0700

    Basic README is being generated

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const questions = [
+const inquirer = require("inquirer");
+const axios = require("axios");
+const generate = require("./utils/generateMarkdown");
+const fs = require("fs");
 
+let email;
+let profile;
+
+const questions = [
+    { message: "What is your GitHub username? ", name: "username" },
+    { message: "What is your project's title? ", name: "title" },
+    { message: "Give a description of your project: ", name: "description" },
+    { message: "What are the installation steps? ", name: "install" },
+    { message: "Descibe the use for your project: ", name: "use" },
+    { message: "Provide tests for your application: ", name: "tests" },
+    { message: "How would you like others to contibute to your project? ", name: "contribute" },
+    { message: "What is the license for this project? ", name: "license" }
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) throw err;
+        console.log("README Generated!")
+    })
 }
 
 function init() {
-
+    inquirer.prompt(questions)
+        .then(answers => {
+            axios.get(`https://api.github.com/users/${answers.username}?access_token=496f0199e2f64036cdc23da39c457a58ca1e4676`)
+                .then(response => {
+                    email = response.data.email;
+                    profile = response.data.avatar_url;
+                    answers.email = email;
+                    answers.profile = profile;
+                    const data = generate(answers);
+                    writeToFile("README.md", data);
+                })
+        });
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,36 @@
 function generateMarkdown(data) {
-  return `
-# ${data.title}
+  return  `
+  # ${data.title}  
 
-`;
+  ## Author Info:  
+  ![Profile Picture](${data.profile})  
+  Email: ${data.email}  
+
+  ## Project Description:   
+  ${data.description}  
+
+  ## Table of contents:  
+  [Installation](#installation)  
+  [Usage](#usage)  
+  [Tests](#tests)  
+  [Contributing](#contributing)  
+  [License](#license)  
+
+  ## Installation:<a id=installation></a>   
+  ${data.install}  
+
+  ## Usage:<a id=usage></a>  
+  ${data.use}  
+
+  ## Tests:<a id=tests></a>  
+  ${data.tests}  
+
+  ## Contributing:<a id=contributing></a>  
+  ${data.contribute}  
+
+  ## License:<a id=license></a>  
+  ${data.license}
+  `;
 }
 
 module.exports = generateMarkdown;
